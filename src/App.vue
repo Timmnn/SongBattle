bb7f38d5043447248f436fe2db218d8755929793
timmn <timmmmnn@gmail.com>
2024-05-18 00:26:44 +0200

    stuff

---
+++ src/App.vue
@@ -0,0 +1,276 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import Tree from "./components/Tree.vue";
+
+const playlist_url = ref("https://open.spotify.com/playlist/7jm9uTjAE8kq7Pvk6s5qko");
+const songs = ref([]);
+const numberOfSongs = ref(0);
+
+const game = ref(null);
+
+class Game {
+   constructor(songs) {
+      this.songs = songs;
+      this.game_tree = this.buildGameTree();
+
+      console.log(this.game_tree);
+
+      this.current_round = this.findCurrentRound();
+
+      console.log(this.current_round);
+   }
+
+   buildGameTree() {
+      // pick n random songs
+
+      let picked_songs = JSON.parse(JSON.stringify(songs.value));
+      while (picked_songs.length > parseInt(numberOfSongs.value)) {
+         const rand_index = Math.floor(Math.random() * picked_songs.length);
+         picked_songs = picked_songs.filter((_, index) => index !== rand_index);
+      }
+
+      const depth = Math.log2(parseInt(numberOfSongs.value));
+
+      while (picked_songs.length > 1) {
+         const left = picked_songs.shift();
+         const right = picked_songs.shift();
+
+         const battle = {
+            round_name: `Round ${depth - Math.log2(picked_songs.length)}`,
+            left,
+            right,
+         };
+
+         left.parent = battle;
+         right.parent = battle;
+
+         console.log(battle);
+
+         picked_songs.push(battle);
+      }
+
+      console.log(picked_songs);
+
+      return picked_songs[0];
+   }
+
+   setWinner(winner_id) {
+      const winner =
+         this.current_round.left.id === winner_id
+            ? this.current_round.left
+            : this.current_round.right;
+
+      if (this.current_round.parent) {
+         if (this.current_round.parent.left === this.current_round) {
+            this.current_round.parent.left = winner;
+         } else {
+            this.current_round.parent.right = winner;
+         }
+      } else {
+         alert("Winner: " + winner.name);
+      }
+      console.log(this.game_tree);
+
+      this.current_round = this.findCurrentRound();
+   }
+
+   findCurrentRound() {
+      // find deepest round
+
+      let current_round = this.game_tree;
+      while (current_round.left.round_name) {
+         current_round = current_round.left;
+      }
+
+      while (current_round.right.round_name) {
+         current_round = current_round.right;
+      }
+
+      console.log("Current Round:", current_round);
+
+      return current_round;
+   }
+}
+
+function newGame() {
+   game.value = new Game(songs.value);
+}
+
+function getAccessToken() {
+   let token = localStorage.getItem("access_token");
+   if (token) {
+      return token;
+   }
+   // get from hash
+   const hash = window.location.hash
+      .substring(1)
+      .split("&")
+      .reduce(function (initial, item) {
+         if (item) {
+            var parts = item.split("=");
+            initial[parts[0]] = decodeURIComponent(parts[1]);
+         }
+         return initial;
+      }, {});
+   token = hash.access_token;
+   if (token) {
+      localStorage.setItem("access_token", token);
+      return token;
+   }
+   return authenticate();
+}
+
+function authenticate() {
+   window.location.href =
+      "https://accounts.spotify.com/authorize?client_id=f30887f7bc6749b99807c2540d408a3c&response_type=token&redirect_uri=https://eternal-penguin-eminently.ngrok-free.app";
+}
+
+function getPlaylist() {
+   const id = playlist_url.value.split("/").pop().split("?")[0];
+
+   fetch("https://api.spotify.com/v1/playlists/" + id, {
+      headers: {
+         Authorization: "Bearer " + getAccessToken(),
+      },
+   })
+      .then(response => response.json())
+      .then(data => {
+         console.log(data);
+         if (data.error?.status === 401) {
+            localStorage.removeItem("access_token");
+            window.location.reload();
+         }
+         songs.value = data.tracks.items.map(item => {
+            return {
+               name: item.track.name,
+               artist: item.track.artists[0].name,
+               album: item.track.album.name,
+               image: item.track.album.images[0].url,
+               id: item.track.id,
+            };
+         });
+      });
+}
+
+function generatePowersOf2(inputArray) {
+   const n = inputArray.length;
+   const powersOf2 = [];
+   let power = 1;
+   while (power < n) {
+      powersOf2.push(power);
+      power *= 2;
+   }
+   return powersOf2;
+}
+</script>
+
+<template>
+   <div class="load-playlist">
+      <h1>Load Playlist</h1>
+      URL: <input type="text" v-model="playlist_url" />
+      <button @click="getPlaylist">Load</button>
+   </div>
+   <div class="songs">
+      <div v-for="song in songs" :key="song.name" class="song">
+         <img :src="song.image" />
+         <div>
+            <h2>{{ song.name }}</h2>
+            <p>{{ song.artist }} - {{ song.album }}</p>
+         </div>
+      </div>
+   </div>
+
+   <div class="new-game" v-if="songs.length">
+      <h1>New Game</h1>
+      <label>
+         Anzahl der Songs:
+         <select v-model="numberOfSongs">
+            <option v-for="n in generatePowersOf2(songs)" :key="n" :value="n">
+               {{ n }}
+            </option>
+         </select>
+      </label>
+
+      <button @click="newGame">Start</button>
+   </div>
+
+   <div class="game-tree">
+      <h1>Game Tree</h1>
+      <Tree v-if="game" :tree="game.game_tree" />
+   </div>
+
+   <div class="current-round" v-if="game?.current_round">
+      <h1>Current Round</h1>
+      <div>
+         <div>
+            <div class="left">
+               <iframe
+                  style="border-radius: 12px"
+                  :src="`https://open.spotify.com/embed/track/${game.current_round.left.id}?utm_source=generator&theme=0`"
+                  width="100%"
+                  height="152"
+                  frameBorder="0"
+                  allowfullscreen=""
+                  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                  loading="lazy"></iframe>
+            </div>
+            <div class="right">
+               <iframe
+                  style="border-radius: 12px"
+                  :src="`https://open.spotify.com/embed/track/${game.current_round.right.id}?utm_source=generator&theme=0`"
+                  width="100%"
+                  height="152"
+                  frameBorder="0"
+                  allowfullscreen=""
+                  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                  loading="lazy"></iframe>
+            </div>
+         </div>
+         <button @click="game.setWinner(game.current_round.left.id)">Left</button>
+         <button @click="game.setWinner(game.current_round.right.id)">Right</button>
+      </div>
+   </div>
+</template>
+
+<style scoped>
+.songs {
+   display: grid;
+   grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+   gap: 1rem;
+   width: 1000px;
+   border-radius: 12px;
+
+   .song {
+      border: 1px solid #ccc;
+      border-radius: 12px;
+      padding: 1rem;
+   }
+
+   img {
+      border-radius: 12px;
+      width: 100%;
+      aspect-ratio: 1;
+   }
+}
+
+.current-round {
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+
+   img {
+      border-radius: 12px;
+      width: 100px;
+      aspect-ratio: 1;
+   }
+
+   button {
+      margin: 1rem;
+      padding: 1rem;
+      border-radius: 12px;
+      border: none;
+      background-color: #007bff;
+      color: white;
+   }
+}
+</style>
---
+++ src/components/Tree.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+const props = defineProps<{
+   tree: any;
+}>();
+</script>
+
+<template>
+   <div class="tree">
+      <div class="node">{{ tree.round_name || tree.name }}</div>
+      <div class="children">
+         <div class="left">
+            <Tree v-if="tree.left" :tree="tree.left" />
+         </div>
+         <div class="right">
+            <Tree v-if="tree.right" :tree="tree.right" />
+         </div>
+      </div>
+   </div>
+</template>
+
+<style scoped lang="scss">
+.tree {
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   border: 1px solid black;
+   padding: 1rem;
+   width: fit-content;
+}
+.children {
+   display: flex;
+   justify-content: space-between;
+}
+</style>
