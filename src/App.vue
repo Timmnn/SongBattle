6a25d4e2f150f087cf39908ec092b62abdd9682d
timmn <timmmmnn@gmail.com>
2024-05-18 20:31:07 +0200

    dfsjk

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import Tree from "./components/Tree.vue";
 
-const playlist_url = ref("https://open.spotify.com/playlist/7jm9uTjAE8kq7Pvk6s5qko");
+const playlist_url = ref("https://open.spotify.com/playlist/37i9dQZF1DWY5ai7gxfuaS");
 const songs = ref([]) as any;
 const numberOfSongs = ref(0);
 
@@ -12,6 +12,8 @@ class Game {
    songs: any;
    game_tree: any;
    current_round: any;
+   current_round_path: boolean[] = [];
+   seen_songs: any = [];
 
    constructor(songs: any) {
       this.songs = songs;
@@ -33,7 +35,12 @@ class Game {
          picked_songs = picked_songs.filter((_: any, index: number) => index !== rand_index);
       }
 
+      // shuffle the songs
+      picked_songs = picked_songs.sort(() => Math.random() - 0.5);
+
       const depth = Math.log2(numberOfSongs.value);
+
+      this.current_round_path = new Array(depth - 1).fill(false);
 
       while (picked_songs.length > 1) {
          const left = picked_songs.shift();
@@ -45,8 +52,8 @@ class Game {
             right,
          };
 
-         left.parent = battle;
-         right.parent = battle;
+         battle.left.parent = battle;
+         battle.right.parent = battle;
 
          console.log(battle);
 
@@ -64,16 +71,27 @@ class Game {
             ? this.current_round.left
             : this.current_round.right;
 
-      if (this.current_round.parent) {
-         if (this.current_round.parent.left === this.current_round) {
-            this.current_round.parent.left = winner;
-         } else {
-            this.current_round.parent.right = winner;
-         }
-      } else {
+      let parent = this.current_round.parent;
+
+      winner.parent = winner.parent.parent;
+
+      if (this.game_tree.left.name && this.game_tree.right.name) {
          alert("Winner: " + winner.name);
+         console.log("WINNERXXX", this.game_tree);
+
+         game.value = null;
+         return;
       }
-      console.log(this.game_tree);
+
+      if (parent) {
+         if (parent.left === this.current_round) {
+            parent.left = winner;
+         } else {
+            parent.right = winner;
+         }
+      }
+
+      console.log("WINNER", this.game_tree);
 
       this.current_round = this.findCurrentRound();
    }
@@ -81,34 +99,48 @@ class Game {
    findCurrentRound() {
       let current_round = this.game_tree;
 
-      console.log("AAA", current_round);
-      while (true) {
-         if (current_round.left.round_name) {
-            current_round = current_round.left;
-            continue;
-         }
+      console.log("PATH", JSON.stringify(this.current_round_path));
 
-         if (current_round.right.round_name) {
-            current_round = current_round.right;
-            continue;
+      //walk down the tree
+      for (let i = 0; i < this.current_round_path.length; i++) {
+         const direction = this.current_round_path[i];
+         current_round = direction ? current_round.right : current_round.left;
+      }
+
+      // if we are at the end of the tree, shorten the path
+      if (!this.current_round_path.some(path => !path) && this.current_round_path.length > 0) {
+         this.current_round_path = new Array(this.current_round_path.length - 1).fill(false);
+         return current_round;
+      }
+
+      // edit the path
+      for (let i = this.current_round_path.length - 1; i >= 0; i--) {
+         this.current_round_path[i] = !this.current_round_path[i];
+         if (this.current_round_path[i]) {
+            break;
          }
-         break;
       }
 
       return current_round;
    }
 }
 
-function getSpotifyAuthUrl() {
-   return `https://accounts.spotify.com/authorize?client_id=f30887f7bc6749b99807c2540d408a3c&response_type=token&redirect_uri=${window.location.origin}`;
-}
-
 function newGame() {
    game.value = new Game(songs.value);
 }
 
+onMounted(() => {
+   getAccessToken();
+});
+
 function getAccessToken() {
    let token = localStorage.getItem("access_token") as any;
+   const expires_at = localStorage.getItem("expires_at");
+   if (expires_at && Date.now() > parseInt(expires_at)) {
+      localStorage.removeItem("access_token");
+      localStorage.removeItem("expires_at");
+      return authenticate();
+   }
    if (token) {
       return token;
    }
@@ -127,6 +159,7 @@ function getAccessToken() {
    token = hash.access_token;
    if (token) {
       localStorage.setItem("access_token", token);
+      localStorage.setItem("expires_at", (Date.now() + 3600 * 1000).toString());
       return token;
    }
    return authenticate();
@@ -167,7 +200,7 @@ function getPlaylist() {
 function generatePowersOf2(inputArray: any) {
    const n = inputArray.length;
    const powersOf2 = [];
-   let power = 1;
+   let power = 2;
    while (power < n) {
       powersOf2.push(power);
       power *= 2;
@@ -202,22 +235,23 @@ function importSongs() {
 </script>
 
 <template>
-   {{ getSpotifyAuthUrl() }}
    <div class="load-playlist">
       <h1>Load Playlist</h1>
-      URL: <input type="text" v-model="playlist_url" />
+      <label>URL: <input type="text" v-model="playlist_url" class="playlist-url-input" /></label>
       <button @click="getPlaylist">Load</button>
    </div>
    <div class="songs">
-      <button @click="exportSongs">Export</button>
-      <button @click="importSongs">Import</button>
+      <button @click="exportSongs" v-if="songs.length">Export Song List</button>
+      <button @click="importSongs">Import Song List</button>
 
       <div v-for="song in songs" class="song">
-         <img :src="song.image" />
-         <div>
-            <h2>{{ song.name }}</h2>
-            <p>{{ song.artist }} - {{ song.album }}</p>
-         </div>
+         <a :href="`https://open.spotify.com/track/${song.id}`" target="_blank">
+            <img :src="song.image" />
+            <div>
+               <h5>{{ song.name }}</h5>
+               <p>{{ song.artist }} - {{ song.album }}</p>
+            </div>
+         </a>
       </div>
    </div>
 
@@ -235,36 +269,37 @@ function importSongs() {
       <button @click="newGame">Start</button>
    </div>
 
-   <div class="game-tree">
+   <div class="game-tree" v-if="game">
       <h1>Game Tree</h1>
-      <Tree v-if="game" :tree="game.game_tree" class="tree" />
+      <Tree :tree="game.game_tree" class="tree" />
    </div>
 
    <div class="current-round" v-if="game?.current_round">
       <h1>Current Round</h1>
-      <div>
-         <div>
-            <div class="left">
-               <iframe
-                  style="border-radius: 12px"
-                  :src="`https://open.spotify.com/embed/track/${game.current_round.left.id}?utm_source=generator&theme=0`"
-                  width="100%"
-                  height="152"
-                  frameBorder="0"
-                  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-                  loading="lazy"></iframe>
-            </div>
-            <div class="right">
-               <iframe
-                  style="border-radius: 12px"
-                  :src="`https://open.spotify.com/embed/track/${game.current_round.right.id}?utm_source=generator&theme=0`"
-                  width="100%"
-                  height="152"
-                  frameBorder="0"
-                  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-                  loading="lazy"></iframe>
-            </div>
+
+      <div class="battle">
+         <div class="left">
+            <iframe
+               style="border-radius: 12px"
+               :src="`https://open.spotify.com/embed/track/${game.current_round.left.id}?utm_source=generator&theme=0`"
+               width="100%"
+               height="152"
+               frameBorder="0"
+               allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+               loading="lazy"></iframe>
          </div>
+         <div class="right">
+            <iframe
+               style="border-radius: 12px"
+               :src="`https://open.spotify.com/embed/track/${game.current_round.right.id}?utm_source=generator&theme=0`"
+               width="100%"
+               height="152"
+               frameBorder="0"
+               allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+               loading="lazy"></iframe>
+         </div>
+      </div>
+      <div class="options">
          <button @click="game.setWinner(game.current_round.left.id)">Left</button>
          <button @click="game.setWinner(game.current_round.right.id)">Right</button>
       </div>
@@ -272,9 +307,24 @@ function importSongs() {
 </template>
 
 <style scoped>
+.playlist-url-input {
+   width: 500px;
+   height: 2rem;
+   margin-right: 1rem;
+}
+
+.battle {
+   display: flex;
+   gap: 1rem;
+   width: 100%;
+}
+.left,
+.right {
+   flex-grow: 1;
+}
 .songs {
    display: grid;
-   grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+   grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
    gap: 1rem;
    width: 1000px;
    border-radius: 12px;
@@ -283,6 +333,11 @@ function importSongs() {
       border: 1px solid #ccc;
       border-radius: 12px;
       padding: 1rem;
+      max-height: 280px;
+      a {
+         text-decoration: none;
+         color: initial;
+      }
    }
 
    img {
@@ -293,13 +348,14 @@ function importSongs() {
 }
 
 .tree {
-   max-width: 100vw;
+   width: 500px;
 }
 
 .current-round {
    display: flex;
    flex-direction: column;
    align-items: center;
+   width: 100%;
 
    img {
       border-radius: 12px;
@@ -315,5 +371,15 @@ function importSongs() {
       background-color: #007bff;
       color: white;
    }
+}
+
+h5,
+p {
+   margin: 0;
+}
+
+.options {
+   display: flex;
+   gap: 1rem;
 }
 </style>
--- src/components/Tree.vue
+++ src/components/Tree.vue
@@ -6,7 +6,6 @@ const props = defineProps<{
 
 <template>
    <div class="tree">
-      <div class="node">{{ props.tree.round_name || tree.name }}</div>
       <div class="children">
          <div class="left">
             <Tree v-if="tree.left" :tree="tree.left" />
@@ -15,20 +14,61 @@ const props = defineProps<{
             <Tree v-if="tree.right" :tree="tree.right" />
          </div>
       </div>
+      <div class="node">
+         <div class="name">
+            {{ props.tree.round_name ? "?" : props.tree.name }}
+         </div>
+      </div>
    </div>
 </template>
 
 <style scoped lang="scss">
 .tree {
    display: flex;
-   flex-direction: column;
-   align-items: center;
+   flex-direction: row;
+   align-items: stretch;
+   width: 100%;
+   gap: 1rem;
+   height: fit-content;
    border: 1px solid black;
-   padding: 1rem;
-   width: fit-content;
 }
+
 .children {
    display: flex;
+   flex-direction: column;
    justify-content: space-between;
+   flex-grow: 1;
+   gap: 1rem;
+}
+
+.lines {
+   display: flex;
+   justify-content: center;
+   align-items: center;
+
+   .up {
+      width: 1px;
+      height: 1rem;
+      background-color: black;
+   }
+
+   .horizontal {
+      width: 1rem;
+      height: 1px;
+      background-color: black;
+   }
+}
+
+.node {
+   flex-grow: 1;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+   .name {
+      border: 1px solid black;
+      background-color: rgb(14, 24, 29);
+      padding: 1rem;
+   }
 }
 </style>
